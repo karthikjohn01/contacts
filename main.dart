567d0d1c0efb0c0b244b7504dbf9c7329c8304eb
karthikjohn01 <KARTHIKOFFICIAL01@GMAIL.COM>
2025-04-15 18:04:04 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,315 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Contact List"),
+        backgroundColor: Colors.orange,
+      ),
+      body: ListView
+      (
+        children: [
+          Container(
+
+        height: 100,
+        padding: EdgeInsets.all(10),
+        margin: EdgeInsets.all(10),
+        width: double.infinity,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Colors.white, 
+        boxShadow:[
+          BoxShadow(blurRadius: 5, spreadRadius: 3, offset: Offset(0, 5), color: Colors.black26)
+        ]),
+        child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          
+          Text("karthik", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+          Text("8072233873")
+            
+            
+          
+        ],
+      ),),
+      Container(
+
+        height: 100,
+        padding: EdgeInsets.all(10),
+        margin: EdgeInsets.all(10),
+        width: double.infinity,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Colors.white, 
+        boxShadow:[
+          BoxShadow(blurRadius: 5, spreadRadius: 3, offset: Offset(0, 5), color: Colors.black26)
+        ]),
+        child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          
+          Text("karthik", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+          Text("8072233873")
+            
+            
+          
+        ],
+      ),),
+      Container(
+
+        height: 100,
+        padding: EdgeInsets.all(10),
+        margin: EdgeInsets.all(10),
+        width: double.infinity,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Colors.white, 
+        boxShadow:[
+          BoxShadow(blurRadius: 5, spreadRadius: 3, offset: Offset(0, 5), color: Colors.black26)
+        ]),
+        child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          
+          Text("karthik", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+          Text("8072233873")
+            
+            
+          
+        ],
+      ),),
+      Container(
+
+        height: 100,
+        padding: EdgeInsets.all(10),
+        margin: EdgeInsets.all(10),
+        width: double.infinity,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Colors.white, 
+        boxShadow:[
+          BoxShadow(blurRadius: 5, spreadRadius: 3, offset: Offset(0, 5), color: Colors.black26)
+        ]),
+        child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          
+          Text("karthik", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+          Text("8072233873")
+            
+            
+          
+        ],
+      ),),
+      Container(
+
+        height: 100,
+        padding: EdgeInsets.all(10),
+        margin: EdgeInsets.all(10),
+        width: double.infinity,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Colors.white, 
+        boxShadow:[
+          BoxShadow(blurRadius: 5, spreadRadius: 3, offset: Offset(0, 5), color: Colors.black26)
+        ]),
+        child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          
+          Text("karthik", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+          Text("8072233873")
+            
+            
+          
+        ],
+      ),),
+      Container(
+
+        height: 100,
+        padding: EdgeInsets.all(10),
+        margin: EdgeInsets.all(10),
+        width: double.infinity,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Colors.white, 
+        boxShadow:[
+          BoxShadow(blurRadius: 5, spreadRadius: 3, offset: Offset(0, 5), color: Colors.black26)
+        ]),
+        child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          
+          Text("karthik", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+          Text("8072233873")
+            
+            
+          
+        ],
+      ),),
+      Container(
+
+        height: 100,
+        padding: EdgeInsets.all(10),
+        margin: EdgeInsets.all(10),
+        width: double.infinity,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Colors.white, 
+        boxShadow:[
+          BoxShadow(blurRadius: 5, spreadRadius: 3, offset: Offset(0, 5), color: Colors.black26)
+        ]),
+        child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          
+          Text("karthik", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+          Text("8072233873")
+            
+            
+          
+        ],
+      ),),
+      Container(
+
+        height: 100,
+        padding: EdgeInsets.all(10),
+        margin: EdgeInsets.all(10),
+        width: double.infinity,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Colors.white, 
+        boxShadow:[
+          BoxShadow(blurRadius: 5, spreadRadius: 3, offset: Offset(0, 5), color: Colors.black26)
+        ]),
+        child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          
+          Text("karthik", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+          Text("8072233873")
+            
+            
+          
+        ],
+      ),),
+      Container(
+
+        height: 100,
+        padding: EdgeInsets.all(10),
+        margin: EdgeInsets.all(10),
+        width: double.infinity,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Colors.white, 
+        boxShadow:[
+          BoxShadow(blurRadius: 5, spreadRadius: 3, offset: Offset(0, 5), color: Colors.black26)
+        ]),
+        child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          
+          Text("karthik", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+          Text("8072233873")
+            
+            
+          
+        ],
+      ),),
+      Container(
+
+        height: 100,
+        padding: EdgeInsets.all(10),
+        margin: EdgeInsets.all(10),
+        width: double.infinity,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Colors.white, 
+        boxShadow:[
+          BoxShadow(blurRadius: 5, spreadRadius: 3, offset: Offset(0, 5), color: Colors.black26)
+        ]),
+        child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          
+          Text("karthik", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+          Text("8072233873")
+            
+            
+          
+        ],
+      ),),
+      Container(
+
+        height: 100,
+        padding: EdgeInsets.all(10),
+        margin: EdgeInsets.all(10),
+        width: double.infinity,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Colors.white, 
+        boxShadow:[
+          BoxShadow(blurRadius: 5, spreadRadius: 3, offset: Offset(0, 5), color: Colors.black26)
+        ]),
+        child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          
+          Text("karthik", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+          Text("8072233873")
+            
+            
+          
+        ],
+      ),),
+      Container(
+
+        height: 100,
+        padding: EdgeInsets.all(10),
+        margin: EdgeInsets.all(10),
+        width: double.infinity,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Colors.white, 
+        boxShadow:[
+          BoxShadow(blurRadius: 5, spreadRadius: 3, offset: Offset(0, 5), color: Colors.black26)
+        ]),
+        child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          
+          Text("karthik", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+          Text("8072233873")
+            
+            
+          
+        ],
+      ),),
+      Container(
+
+        height: 100,
+        padding: EdgeInsets.all(10),
+        margin: EdgeInsets.all(10),
+        width: double.infinity,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Colors.white, 
+        boxShadow:[
+          BoxShadow(blurRadius: 5, spreadRadius: 3, offset: Offset(0, 5), color: Colors.black26)
+        ]),
+        child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          
+          Text("karthik", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+          Text("8072233873")
+            
+            
+          
+        ],
+      ),),
+      Container(
+
+        height: 100,
+        padding: EdgeInsets.all(10),
+        margin: EdgeInsets.all(10),
+        width: double.infinity,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Colors.white, 
+        boxShadow:[
+          BoxShadow(blurRadius: 5, spreadRadius: 3, offset: Offset(0, 5), color: Colors.black26)
+        ]),
+        child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          
+          Text("karthik", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+          Text("8072233873")
+            
+            
+          
+        ],
+      ),),
+        ],
+      )
+
+    ),
+  ));
+
+  
+}
